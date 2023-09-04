c1fdf8e475e7cace36e5f80ff0c37e123f135cf0
jultty <juno.t@tuta.io>
2023-09-03 23:45:04 -0300

    Implement core logic

--- app/src/main/java/paren/sop/App.java
+++ app/src/main/java/paren/sop/App.java
@@ -1,11 +1,67 @@
 package paren.sop;
 
+import java.util.Stack;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+
+  public static void main(String[] args) {
+
+    Stack P = new Stack();
+
+    String valid_expression = "((((((((((()))))))))))";
+    String invalid_expression = "((((((((())))))))";
+
+    boolean valid_output = evaluateExpression(P, valid_expression);
+    boolean invalid_output = evaluateExpression(P, invalid_expression);
+
+    System.out.println("valid_output: " + valid_output);
+    System.out.println("invalid_output: " + invalid_output);
+
+  }
+
+  // Criar a função pop(P, sym)
+  // empilha na pilha P um caractere lido da expressão analisada
+  // parâmetros: a pilha P e o caractere lido sym
+  public static void pop(Stack p, char sym) { p.push(sym); }
+
+  // Criar a função top()
+  // retorno: o caractere empilhado no topo da pilha P
+  // parâmetro: a pilha P
+  public static char top(Stack p) { return (char) p.peek(); }
+
+  public static boolean evaluateExpression(Stack P, String expression) {
+
+    // Para cada caractere sym na expressão (percorrendo da esq. para direita) repita:
+    for (int i = 0; i < expression.length(); i++) {
+
+      char sym = expression.charAt(i);
+
+      // 5.1 Se sym é um parêntesis esquerdo
+      if (sym == '(') {
+        // 5.1.1 Adicione sym ao topo da pilha P
+        pop(P, sym);
+      }
+
+      // 5.2 Se sym é um parêntesis direito
+      else if (sym == ')') {
+        // 5.2.1 Se a pilha P está vazia, termine com false.
+        if (P.isEmpty()) {
+          return false;
+        }
+
+        // 5.2.2 Remova o parêntesis do topo de P para uma variável left.
+        char left = top(P);
+        P.pop();
+
+        // 5.2.3 Se left e sym não são parêntesis complementares, termine com false.
+        if (left == sym) {
+          return false;
+
+        }
+      }
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+    // Termine com true se a pilha P está vazia, ou false caso contrário
+    if (P.isEmpty()) { return true; } else { return false; }
+  }
 }
---
+++ dev.sh
@@ -0,0 +1,7 @@
+while true; do
+clear
+gradle test --rerun --fail-fast
+gradle run
+echo
+inotifywait -r -e modify .
+done
